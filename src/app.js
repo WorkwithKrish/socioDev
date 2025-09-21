74091d96b962c09b9afacf8038110461925cdc40
Krishna <manifester2@outlook.com>
2025-09-21 12:33:13 +0530

    create a server

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use("/home", (req, res) => {
+  res.send("Hello world kk");
 });
-
+app.get("/", (req, res) => {
+  res.send("Hello world"); //last
+});
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
